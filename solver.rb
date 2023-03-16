a5ce983eae043f7258691fc5aa1ca564f8f469fe
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-16 17:26:25 +0530

    Refactered the code

--- solver.rb
+++ solver.rb
@@ -1,16 +1,16 @@
 class Solver
-    def initialize(number)
-        @number = number
-    end
+    # def initialize(number)
+    #     number = number
+    # end
 
-    def factorial
-        if @number < 0
+    def factorial(number)
+        if number < 0
             return 'Plese give me a positive number'
-        elsif @number == 0
+        elsif number == 0
             return 1
         else
             ans = 1
-            (1..@number).each {|i|
+            (1..number).each {|i|
                 ans *= i
             }
             return ans
