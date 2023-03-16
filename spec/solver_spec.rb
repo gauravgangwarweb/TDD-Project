fa44b093fe5d8ed5073adb24b95b349abe564d5f
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-16 16:56:20 +0530

    create an instance of solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,6 +2,7 @@ require './solver'
 
 describe Solver do
     context 'should return the factorial' do
+        solver = Solver.new(5)
         it 'returns correct factorial' do
             expect(solver.factorial).to eq(120)
         end
