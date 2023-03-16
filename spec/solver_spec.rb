b65cda0ba57d053a070fc09168ac7e1c46fdcde1
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-16 17:10:27 +0530

    Add new test cases for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,19 @@
 require './solver'
 
 describe Solver do
-    context 'should return the factorial' do
-        solver = Solver.new(5)
-        it 'returns correct factorial' do
+    context 'should create instance of solver class' do
+        it 'returns error message if number is negative' do
+            solver = Solver.new(-1)
+            expect(solver.factorial).to eq('Plese give me a positive number')
+        end
+
+        it 'returns 1 if number is equal to 0' do
+            solver = Solver.new(0)
+            expect(solver.factorial).to eq(1)
+        end
+
+        it 'returns correct factorial if number is positive' do
+            solver = Solver.new(5)
             expect(solver.factorial).to eq(120)
         end
     end
