9aa59cc3f6b7fbc3834645e26e074c59eac64a5a
Prantosh Biswas <pbiswas1007@gmail.com>
2023-03-16 18:44:31 +0530

    Fix Linters Errors

--- solver.rb
+++ solver.rb
@@ -1,33 +1,31 @@
 class Solver
+  def factorial(number)
+    if number.negative?
+      'Plese give me a positive number'
+    elsif number.zero?
+      1
+    else
+      ans = 1
+      (1..number).each do |i|
+        ans *= i
+      end
+      ans
+    end
+  end
 
-    def factorial(number)
-        if number < 0
-            return 'Plese give me a positive number'
-        elsif number == 0
-            return 1
-        else
-            ans = 1
-            (1..number).each {|i|
-                ans *= i
-            }
-            return ans
-        end
-    end
-    
-    def reverse(str)
-        ans = str.reverse
-        return ans
-    end
+  def reverse(str)
+    str.reverse
+  end
 
-    def fizzbuzz(number)
-        if number % 3 == 0 && number % 5 == 0
-            return 'fizzbuzz'
-        elsif number % 3 == 0
-            return 'fizz'
-        elsif number % 5 == 0
-            return 'buzz'
-        else
-            return number
-        end
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number
     end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,50 +1,50 @@
 require './solver'
 
 describe Solver do
-    solver = Solver.new()
+  solver = Solver.new
 
-    describe '#factorial' do
-        context 'should returns the correct factorials' do
-            it 'returns error message if number is negative' do
-                expect(solver.factorial(-2)).to eq('Plese give me a positive number')
-            end
+  describe '#factorial' do
+    context 'should returns the correct factorials' do
+      it 'returns error message if number is negative' do
+        expect(solver.factorial(-2)).to eq('Plese give me a positive number')
+      end
 
-            it 'returns 1 if number is equal to 0' do
-                expect(solver.factorial(0)).to eq(1)
-            end
+      it 'returns 1 if number is equal to 0' do
+        expect(solver.factorial(0)).to eq(1)
+      end
 
-            it 'returns correct factorial if number is positive' do
-                expect(solver.factorial(5)).to eq(120)
-            end
-        end
+      it 'returns correct factorial if number is positive' do
+        expect(solver.factorial(5)).to eq(120)
+      end
     end
+  end
 
-    describe '#reverse' do
-        context 'should returns the correct reversed word' do
-            it 'returns metsys if word is system' do
-                expect(solver.reverse('system')).to eq('metsys')
-            end
-        end
+  describe '#reverse' do
+    context 'should returns the correct reversed word' do
+      it 'returns metsys if word is system' do
+        expect(solver.reverse('system')).to eq('metsys')
+      end
     end
+  end
 
 
-    describe '#fizzbuzz' do
-        context 'should returns the correct fizzbuzz' do
-            it 'returns 1 if number is equal to 1' do
-                expect(solver.fizzbuzz(1)).to eq(1)
-            end
+  describe '#fizzbuzz' do
+    context 'should returns the correct fizzbuzz' do
+      it 'returns 1 if number is equal to 1' do
+        expect(solver.fizzbuzz(1)).to eq(1)
+      end
 
-            it 'returns fizz if number is divisible by 3' do
-                expect(solver.fizzbuzz(3)).to eq('fizz')
-            end
+      it 'returns fizz if number is divisible by 3' do
+        expect(solver.fizzbuzz(3)).to eq('fizz')
+      end
 
-            it 'returns buzz if number is divisible by 5' do
-                expect(solver.fizzbuzz(5)).to eq('buzz')
-            end
+      it 'returns buzz if number is divisible by 5' do
+        expect(solver.fizzbuzz(5)).to eq('buzz')
+      end
 
-            it 'returns fizzbuzz if number is divisible by 3 and 5' do
-                expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-            end
-        end
+      it 'returns fizzbuzz if number is divisible by 3 and 5' do
+        expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      end
     end
+  end
 end
