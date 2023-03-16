4a619c595f536444125385b4d75c575999fe14b0
Prantosh Biswas <pbiswas1007@gmail.com>
2023-03-16 18:38:20 +0530

    Add test case for FizzBuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -26,4 +26,25 @@ describe Solver do
             end
         end
     end
+
+
+    describe '#fizzbuzz' do
+        context 'should returns the correct fizzbuzz' do
+            it 'returns 1 if number is equal to 1' do
+                expect(solver.fizzbuzz(1)).to eq(1)
+            end
+
+            it 'returns fizz if number is divisible by 3' do
+                expect(solver.fizzbuzz(3)).to eq('fizz')
+            end
+
+            it 'returns buzz if number is divisible by 5' do
+                expect(solver.fizzbuzz(5)).to eq('buzz')
+            end
+
+            it 'returns fizzbuzz if number is divisible by 3 and 5' do
+                expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+            end
+        end
+    end
 end
