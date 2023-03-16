5f5a64ec59637e4fb0bb8a1f1cd03f9f6908c091
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-16 17:25:49 +0530

    refectred the test cases for factorial fuction

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,20 +1,20 @@
 require './solver'
 
 describe Solver do
-    context 'should create instance of solver class' do
-        it 'returns error message if number is negative' do
-            solver = Solver.new(-1)
-            expect(solver.factorial).to eq('Plese give me a positive number')
-        end
+    describe '#factorial' do
+        context 'should returns the correct factorials' do
+            solver = Solver.new()
+            it 'returns error message if number is negative' do
+                expect(solver.factorial(-2)).to eq('Plese give me a positive number')
+            end
 
-        it 'returns 1 if number is equal to 0' do
-            solver = Solver.new(0)
-            expect(solver.factorial).to eq(1)
-        end
+            it 'returns 1 if number is equal to 0' do
+                expect(solver.factorial(0)).to eq(1)
+            end
 
-        it 'returns correct factorial if number is positive' do
-            solver = Solver.new(5)
-            expect(solver.factorial).to eq(120)
+            it 'returns correct factorial if number is positive' do
+                expect(solver.factorial(5)).to eq(120)
+            end
         end
     end
 end
